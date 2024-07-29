27fc529eab4ab8232dc63efa5265883459071014
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-07-29 09:27:33 +0000

    new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import "../styles/index.css";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <SessionProvider session={pageProps.session}>
-    <Component {...pageProps} />;
-  </SessionProvider>;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
