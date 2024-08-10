41cf1fc60674243505225c36b270f8f84927d999
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-08-10 18:01:18 +0000

    handle signing out button

--- pages/protected/index.tsx
+++ pages/protected/index.tsx
@@ -1,25 +1,33 @@
 "use client";
 import { useSession, signOut, signIn } from "next-auth/react";
 import { Button } from "../../components/ui/button";
+import { useState } from "react";
 
 function ProtectedRoute() {
   const { data: session, status } = useSession();
+  const [isSigningOut, setIsSigningOut] = useState(false);
+
   console.log(session);
 
   if (status === "loading") {
     return <div>Loading...</div>;
   }
 
+  const handleSignOut = () => {
+    setIsSigningOut(true);
+    signOut({ callbackUrl: "/" });
+  };
   return (
     <>
       {session ? (
         <div className="h-screen flex items-center justify-center">
           Hello and welcome {session.user.name}
           <Button
-            onClick={() => signOut({ callbackUrl: "/" })}
+            onClick={handleSignOut}
             className="mx-10"
+            disabled={isSigningOut}
           >
-            Sign out
+            {isSigningOut ? "Signing out" : "Sign out"}
           </Button>
         </div>
       ) : (
