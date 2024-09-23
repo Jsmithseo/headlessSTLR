cd2ae7732ab820536d1a7317e2651079b2d40d8b
Nicholas <nickigber@gmail.com>
2024-09-23 21:05:15 +0100

    create artist dashboard

---
+++ pages/dashboard/settings.js
@@ -0,0 +1,13 @@
+// pages/dashboard/settings.js
+import DashboardLayout from '../../components/DashboardLayout';
+
+const Settings = () => {
+  return (
+    <DashboardLayout>
+      <h1>Settings</h1>
+      <p>Manage your dashboard settings here.</p>
+    </DashboardLayout>
+  );
+};
+
+export default Settings;
